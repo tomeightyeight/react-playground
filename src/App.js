b447c5316a7fcb3e81550f190f7e369483f2cbe1
Tom Humphris <tom@muska.co.uk>
2017-05-03 21:31:14 +0100

    My first React component with props and defaults

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-'use strict';
-
 import React from 'react';
 
-const App = () => <h1>Hello, World!</h1>;
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Mr first React App</h1>
+        <p>Hello, { this.props.name }!</p>
+        <p>{ this.props.num }</p>
+      </div>
+    );
+  }
+};
+
+App.propTypes = {
+  name: React.PropTypes.string,
+  num: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+  name: 'World',
+  num: 0
+};
 
 export default App;
